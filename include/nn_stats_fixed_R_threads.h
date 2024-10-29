1cc977ac393923f938ffe2da9f0267e878f7ab40
Nicolas B. Garnier <nicolas.garnier@ens-lyon.fr>
2024-10-29 15:31:17 +0100

    multi radii code WIP

--- include/nn_stats_fixed_R_threads.h
+++ include/nn_stats_fixed_R_threads.h
@@ -6,6 +6,7 @@
 /* Nicolas Garnier	nicolas.garnier@ens-lyon.fr			                */
 /************************************************************************/
 
-int compute_stats_fixed_R_threads(double *x, double *A, int npts_in, int nx, int nA, double *y, int npts_out, double R, double *A_mean, double *A_std, int *k);
+int compute_stats_fixed_R_threads(double *x, double *A, int npts_in, int nx, int nA, double *y, int npts_out, double R,          double *A_mean, double *A_std, int *k);
 
+int compute_stats_fixed_R_threads(double *x, double *A, int npts_in, int nx, int nA, double *y, int npts_out, double *R, int nR, double *A_mean, double *A_std, int *k);
 
