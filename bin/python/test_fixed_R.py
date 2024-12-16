afd6920a23e813751ad49c7a1eb78dcb7b9fe4a6
Nicolas B. Garnier <nicolas.garnier@ens-lyon.fr>
2024-12-16 10:28:22 +0100

    fixed k example

--- bin/python/test_fixed_R.py
+++ bin/python/test_fixed_R.py
@@ -25,8 +25,8 @@ matplotlib.rcParams['axes.formatter.use_mathtext'] = True
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # set parameters
-k=10
-R=1.2
+k=30
+R=0.5
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -68,30 +68,38 @@ print("output positions :", y.shape)
 
 # prepare plot
 Fig = plt.figure(figsize=(15,10))
-P1  = Fig.add_subplot(2,3,1); P1.set_title("initial locations"); P1.set_xlim(-radius, radius); P1.set_ylim(-radius, radius)
-P2  = Fig.add_subplot(2,3,2); P2.set_title("fixed k=%d" %k);     P2.set_xlim(-radius, radius); P2.set_ylim(-radius, radius)
-P3  = Fig.add_subplot(2,3,3); P3.set_title("fixed k=%d" %(2*k)); P3.set_xlim(-radius, radius); P3.set_ylim(-radius, radius)
-P4  = Fig.add_subplot(2,3,4); P4.set_title("fixed R=%1.1f" %R);  P4.set_xlim(-radius, radius); P4.set_ylim(-radius, radius)
-P5  = Fig.add_subplot(2,3,5); P5.set_title("fixed k=%d" %(3*k)); P5.set_xlim(-radius, radius); P5.set_ylim(-radius, radius)
-P6  = Fig.add_subplot(2,3,6); P6.set_title("fixed k=%d" %(4*k)); P6.set_xlim(-radius, radius); P6.set_ylim(-radius, radius)
+P1  = Fig.add_subplot(2,3,1); P1.set_title("initial locations");    P1.set_xlim(-radius, radius); P1.set_ylim(-radius, radius)
+P2  = Fig.add_subplot(2,3,2); P2.set_title("fixed R=%1.1f" %R);     P2.set_xlim(-radius, radius); P2.set_ylim(-radius, radius)
+P3  = Fig.add_subplot(2,3,3); P3.set_title("fixed R=%1.1f" %(2*R)); P3.set_xlim(-radius, radius); P3.set_ylim(-radius, radius)
+P4  = Fig.add_subplot(2,3,4); P4.set_title("fixed k=%d" %k);        P4.set_xlim(-radius, radius); P4.set_ylim(-radius, radius)
+P5  = Fig.add_subplot(2,3,5); P5.set_title("fixed R=%1.1f" %(3*R)); P5.set_xlim(-radius, radius); P5.set_ylim(-radius, radius)
+P6  = Fig.add_subplot(2,3,6); P6.set_title("fixed R=%1.1f" %(4*R)); P6.set_xlim(-radius, radius); P6.set_ylim(-radius, radius)
 
 P1.scatter(pos[0], pos[1], marker='.', edgecolor='b', facecolor='none', alpha=0.5 )
 
 # single value of k:
-print("\nfixed k =", 4*k, end=" ")
+print("\nfixed k =", k, end=" ")
 t1=time()
-[A_mean, A_var, dists] = ns.compute_local_stats(pos, val, y, k=np.array([4*k], dtype=np.intc))
+[A_mean, A_var, dists] = ns.compute_local_stats(pos, val, y, k=np.array([k], dtype=np.intc))
 print("\telapsed time", time()-t1)
-P6.scatter(y[0,:].flatten(), y[1,:].flatten(), c=A_mean/dists, marker='o')
+P4.scatter(y[0,:].flatten(), y[1,:].flatten(), c=A_mean/dists, marker='o')
 
-# multiple values of k:
-print("\nfixed k =", [k, 2*k, 3*k, 4*k], end=" ")
+# single value of R:
+print("\nfixed R =", 2*R, end=" ")
 t1=time()
-[A_mean, A_var, dists] = ns.compute_local_stats(pos, val, y, k=np.array([k, 2*k, 3*k, 4*k], dtype=np.intc))
+[A_mean, A_var, nn] = ns.compute_local_stats(pos, val, y, R=np.array([2*R]))
 print("\telapsed time", time()-t1)
-P2.scatter(y[0,:].flatten(), y[1,:].flatten(), c=A_mean[0]/dists[0], marker='o')
-P3.scatter(y[0,:].flatten(), y[1,:].flatten(), c=A_mean[1]/dists[1], marker='o')
-P5.scatter(y[0,:].flatten(), y[1,:].flatten(), c=A_mean[2]/dists[2], marker='o')
+P2.scatter(y[0,:].flatten(), y[1,:].flatten(), c=A_mean*nn, marker='o')
+
+# multiple values of R:
+print("\nfixed R =", [R, 2*R, 3*R, 4*R], end=" ")
+t1=time()
+[A_mean, A_var, nn] = ns.compute_local_stats(pos, val, y, R=np.array([R, 2*R, 3*R, 4*R]))
+print("\telapsed time", time()-t1)
+#P2.scatter(y[0,:].flatten(), y[1,:].flatten(), c=A_mean[0]/dists[0], marker='o')
+P3.scatter(y[0,:].flatten(), y[1,:].flatten(), c=A_mean[1]*nn[1], marker='o')
+P5.scatter(y[0,:].flatten(), y[1,:].flatten(), c=A_mean[2]*nn[2], marker='o')
+P6.scatter(y[0,:].flatten(), y[1,:].flatten(), c=A_mean[3]*nn[3], marker='o')
 print("output values of size", A_mean.shape, A_var.shape, "and", dists.shape)
 #print(dists)
 #print(A_mean)
@@ -99,14 +107,6 @@ print("\telapsed time", time()-t1)
 
 #exit()
 
-t1=time()
-print("\nfixed R =", R, end=" ")
-A_mean, A_var, nnn = ns.compute_local_stats(pos, val, y, R=np.array([R]))
-print("output values of size", A_mean.shape, A_var.shape, "and", nnn.shape)
-print("\telapsed time", time()-t1)
-
-P4.scatter(y[0,:].flatten(), y[1,:].flatten(), c=A_mean*nnn, marker='o')
-print(nnn)
-Fig.savefig("test_fixed_k.pdf")
+Fig.savefig("essai.pdf")
 
 # %%
